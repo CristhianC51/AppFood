3f91b77177a182de0475095b6c706682f0089161
Andres castellanos <acastellanos565@unab.edu.co>
2023-10-21 19:21:20 -0500

    commits de creación wireframes

---
+++ app/src/main/java/com/cristhiancaballero/appfood/Activity_Compras.java
@@ -0,0 +1,14 @@
+package com.cristhiancaballero.appfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Compras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compras);
+    }
+}
